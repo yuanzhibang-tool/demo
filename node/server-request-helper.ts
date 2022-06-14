2516f6aa1dc87b9d8277921804dcc98ed1cb0fcc
shandawei <shandawei@me.com>
2022-06-14 12:39:49 +0000

    完善node

--- node/server-request-helper.ts
+++ node/server-request-helper.ts
@@ -1,4 +1,5 @@
 import got from 'got';
+import { HttpsProxyAgent } from 'hpagent';
 
 interface ServerResponse {
     status: string;
@@ -8,9 +9,27 @@ interface ServerResponse {
 
 class ServerRequestHelper {
     static post(api: string, data: any) {
+        const httpsAgent = new HttpsProxyAgent({
+            keepAlive: true,
+            keepAliveMsecs: 1000,
+            maxSockets: 256,
+            maxFreeSockets: 256,
+            proxy: 'http://proxy_user:F2pkto4GtRPAqTpY@x.orzzzzzz.com:7789'
+        })
+        const httpAgent = new HttpsProxyAgent({
+            keepAlive: true,
+            keepAliveMsecs: 1000,
+            maxSockets: 256,
+            maxFreeSockets: 256,
+            proxy: 'http://proxy_user:F2pkto4GtRPAqTpY@x.orzzzzzz.com:7789'
+        })
         return new Promise<ServerResponse>((resolve, reject) => {
-            got.post('https://baidu.com', {
-                form: data
+            got.post('https://api-service.david-health.cn/api/v1/Ip/getClientIp', {
+                form: data,
+                agent: {
+                    https: httpsAgent,
+                    http: httpAgent
+                }
             }).then((response) => {
                 const stringBody = response.body;
                 const responseObject = JSON.parse(stringBody);
