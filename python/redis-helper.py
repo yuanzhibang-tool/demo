c4af9216abd9c0ae57ee35ed6264f45e1b2329c4
shandawei <shandawei@me.com>
2022-06-14 06:58:41 +0000

    完善方法名

--- python/redis-helper.py
+++ python/redis-helper.py
@@ -10,36 +10,30 @@ class RedisHelper:
 
     # 从数据库获取有效的server_access_token
     def get_server_access_token(self, app_id):
-        client = redis.Redis(
-            host=self.config['host'], port=self.config['port'], username=self.config['username'], password=self.config['auth'], decode_responses=True)
-        server_access_token_info_string = client.get(
-            RedisHelper.get_app_server_access_token_cache_key(app_id))
-        client.close()
-        if server_access_token_info_string:
-            server_access_token_info = json.loads(
-                server_access_token_info_string)
-            if server_access_token_info:
-                return server_access_token_info['server_access_token']
-        return None
+        cache_key = RedisHelper.get_server_access_token_cache_key(app_id)
+        return self.get_redis_json_value_by_key(cache_key, 'server_access_token')
 
     def get_js_ticket(self, app_id):
+        cache_key = RedisHelper.get_js_ticket_cache_key(app_id)
+        return self.get_redis_json_value_by_key(cache_key, 'js_ticket')
+
+    def get_redis_json_value_by_key(self, key, codeKey):
         client = redis.Redis(
             host=self.config['host'], port=self.config['port'], username=self.config['username'], password=self.config['auth'], decode_responses=True)
-        js_ticket_info_string = client.get(
-            RedisHelper.get_app_js_ticket_cache_key(app_id))
+        js_ticket_info_string = client.get(key)
         client.close()
         if js_ticket_info_string:
             js_ticket_info = json.loads(js_ticket_info_string)
             if js_ticket_info:
-                return js_ticket_info['js_ticket']
+                return js_ticket_info[codeKey]
         return None
 
     @staticmethod
-    def get_app_server_access_token_cache_key(app_id):
+    def get_server_access_token_cache_key(app_id):
         return "type/server_access_token_info/app_id/%s" % app_id
 
     @staticmethod
-    def get_app_js_ticket_cache_key(app_id):
+    def get_js_ticket_cache_key(app_id):
         return "type/js_ticket_info/app_id/%s" % app_id
 
 
