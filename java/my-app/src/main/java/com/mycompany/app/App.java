ea23615898c6a9a1d1bf90b404ea97b5d386258d
shandawei <shandawei@me.com>
2022-06-14 09:09:41 +0000

    完善redis缓存类

--- java/my-app/src/main/java/com/mycompany/app/App.java
+++ java/my-app/src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 public class App {
 
     public static void main(String[] args) {
-        // RedisHelper.test();
+        RedisHelper.test();
         ServerRequestHelper.test();
         System.out.println("Hello World!");
         String urlString = "https://yuanzhibang.com/a/b/?x=1&b=2#2"; //
--- java/my-app/src/main/java/com/mycompany/app/RedisHelper.java
+++ java/my-app/src/main/java/com/mycompany/app/RedisHelper.java
@@ -1,16 +1,52 @@
 package com.mycompany.app;
 
+import java.util.Map;
+
+import com.google.gson.Gson;
 import com.lambdaworks.redis.*;
 
 public class RedisHelper {
-    public static void test() {
+    public String redisUrl;
+
+    RedisHelper(String redisUrl) {
+        this.redisUrl = redisUrl;
+    }
+
+    public String getJsTicket(String appId) {
+        String key = RedisHelper.getJsTicketCacheKey(appId);
+        return this.getRedisJsonValueByKey(key, "js_ticket");
+    }
+
+    public String getServerAccessToken(String appId) {
+        String key = RedisHelper.getServerAccessTokenCacheKey(appId);
+        return this.getRedisJsonValueByKey(key, "server_access_token");
+    }
+
+    public static String getServerAccessTokenCacheKey(String appId) {
+        return String.format("type/server_access_token_info/app_id/%s", appId);
+    }
+
+    public static String getJsTicketCacheKey(String appId) {
+        return String.format("type/js_ticket_info/app_id/%s", appId);
+    }
+
+    @SuppressWarnings("unchecked")
+    public String getRedisJsonValueByKey(String key, String codeKey) {
         RedisClient redisClient = new RedisClient(
-                RedisURI.create("redis://password@host:port"));
+                RedisURI.create(this.redisUrl));
         RedisConnection<String, String> connection = redisClient.connect();
-
-        System.out.println("Connected to Redis");
-
+        String jsonInfo = connection.get(key);
+        Gson gson = new Gson();
+        Map<String, String> info = gson.fromJson(jsonInfo, Map.class);
+        String value = info.get(codeKey);
         connection.close();
         redisClient.shutdown();
+        return value;
+    }
+
+    public static void test() {
+        RedisHelper helper = new RedisHelper("redis://p8WOmXgzZg@demo-dev-cache-redis:6379");
+        String jsTicket = helper.getJsTicket("100027");
+        String serverAccessToken = helper.getServerAccessToken("100027");
     }
 }
