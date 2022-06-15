4fe706f225c91ea405bcd5c597f6929674c55ed5
shandawei <shandawei@me.com>
2022-06-15 08:18:59 +0000

    添加接口

--- python/oauth_api_helper.py
+++ python/oauth_api_helper.py
@@ -1,3 +1,4 @@
+from http import server
 from api_request_helper import ApiRequestHelper, OauthApiError, OauthNetworkError
 
 class OauthApiHelper:
@@ -16,9 +17,21 @@ class OauthApiHelper:
 
 
     @staticmethod
+    def get_app_user_count(app_id):
+        api = OauthApiHelper.get_api_by_path("/CommonResource/getUserCount")
+        server_access_token = OauthApiHelper.get_server_access_token(app_id)
+        params = {"app_id":app_id,"access_token":server_access_token}
+        response_data = ApiRequestHelper.post(api,params)
+        return response_data
+
+    @staticmethod
+    def get_server_access_token(app_id):
+        return "658bc3778dd7a8db5209fd16e91b3b8d8d115129d22dc4ab7f42cb4f6394a625"
+    @staticmethod
     def get_api_by_path(path:str):
         api = "https://oauth.yuanzhibang.com" + path
         return api
 
 
 # OauthApiHelper.check_code("101170","d6e8d4288c3ed26d710d3a53a6e86572e7677e93618d69d495a6f38869280fb97","js_ticket")
+OauthApiHelper.get_app_user_count("101170")
