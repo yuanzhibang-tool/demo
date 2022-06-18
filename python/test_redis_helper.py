d323800e23fdf04ad6533742aba33dbe1c1096f2
shandawei <shandawei@me.com>
2022-06-18 04:06:33 +0000

    优化单元测试

--- python/test_redis_helper.py
+++ python/test_redis_helper.py
@@ -1,19 +1,33 @@
 import unittest
 import os
 from redis_helper import RedisHelper
-
+import redis
 app_id = os.getenv('TEST_APP_ID')
 redis_config = {
-    "host": "localhost",
-    "port": 6379
+    "host": "demo-dev-cache-redis",
+    "port": 6379,
+    "auth": 'p8WOmXgzZg',
+    "username": "default"
 }
+
+# !此单元测试需要有docker环境
 
 
 class TestRedisHelper(unittest.TestCase):
     def setUp(self):
-        pass
+        # ! set init code info to redis
+        # os.system('docker stop test-redis-server ; docker rm test-redis-server  ; docker run  -p 6379:6379 -d --name test-redis-server redis')
+        client = redis.Redis(
+            host=redis_config['host'], port=redis_config['port'], username=redis_config['username'], password=redis_config['auth'], decode_responses=True)
+        key = "type/server_access_token_info/app_id/%s" % app_id
+        value = '{"server_access_token": "server_access_token:082675bb1bcbdc3b824fb040abdfd4e4b5e36e422af60365949e17e372cbcd4c", "expires_in": 1654575742}'
+        client.set(key, value)
+        key = "type/js_ticket_info/app_id/%s" % app_id
+        value = '{"js_ticket": "js_ticket:082675bb1bcbdc3b824fb040abdfd4e4b5e36e422af60365949e17e372cbcd4c", "expires_in": 1654575742}'
+        client.set(key, value)
 
     def tearDown(self):
+        # os.system("docker stop test-redis-server ; docker rm test-redis-server;")
         pass
 
     def test_get_server_access_token_cache_key(self):
@@ -29,5 +43,11 @@ class TestRedisHelper(unittest.TestCase):
     def test_get_server_access_token(self):
         helper = RedisHelper(redis_config)
         value = helper.get_server_access_token(app_id)
-        expect_value = ""
+        expect_value = "server_access_token:082675bb1bcbdc3b824fb040abdfd4e4b5e36e422af60365949e17e372cbcd4c"
+        self.assertEqual(value, expect_value)
+
+    def test_get_js_ticket(self):
+        helper = RedisHelper(redis_config)
+        value = helper.get_js_ticket(app_id)
+        expect_value = "js_ticket:082675bb1bcbdc3b824fb040abdfd4e4b5e36e422af60365949e17e372cbcd4c"
         self.assertEqual(value, expect_value)
