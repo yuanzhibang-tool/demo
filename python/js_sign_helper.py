b11bd8d8ef0e909c0c3f8b9efe4b67fafb6a36fc
shandawei <shandawei@me.com>
2022-06-18 04:53:33 +0000

    完善单元测试,优化方法

--- python/js_sign_helper.py
+++ python/js_sign_helper.py
@@ -22,32 +22,30 @@ class JsSignHelper:
         return url
 
     @staticmethod
-    def get_js_ticket_from_redis(appId):
-        pass
-
-    @staticmethod
-    def get_js_sign_info(appId: str, url: str):
-        jsTicket = ""  # !从redis中取出
-        # 取出url中的path之前的部分,参数和锚点不参与计算
-        url = JsSignHelper.get_pure_url(url)
+    def get_js_sign_info(app_id: str, url: str, js_ticket: str):
         # 获取当前时间戳
         timestamp = int(time.time())
         # 生成nonce
         noncestr = JsSignHelper.get_guid()
-        # jsTicket = "a4dcdk"
-        # timestamp = 1654850924
-        # noncestr = '1234'
         # 组合签名字符串
-        string = f"js_ticket=%s&nonce_str=%s&timestamp=%d&url=%s" % (
-            jsTicket, noncestr, timestamp, url)
-        hash_object = hashlib.sha1(string.encode("utf-8"))
-        sign = hash_object.hexdigest()
+        sign = JsSignHelper.get_sign(js_ticket, noncestr, timestamp, url)
         returnData = {}
-        returnData['app_id'] = appId
+        returnData['app_id'] = app_id
         returnData['timestamp'] = f"%d" % timestamp
         returnData['nonce_str'] = noncestr
         returnData['signature'] = sign
+        returnData['url'] = url
         return returnData
+
+    @staticmethod
+    def get_sign(js_ticket, noncestr, timestamp, url):
+        # 取出url中的path之前的部分,参数和锚点不参与计算
+        url = JsSignHelper.get_pure_url(url)
+        sign_string = f"js_ticket=%s&nonce_str=%s&timestamp=%d&url=%s" % (
+            js_ticket, noncestr, timestamp, url)
+        hash_object = hashlib.sha1(sign_string.encode("utf-8"))
+        sign = hash_object.hexdigest()
+        return sign
 
     @staticmethod
     def test():
